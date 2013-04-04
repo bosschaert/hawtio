2ea472ecddb470396698a46d548d0903bd9b8fe6
David Bosschaert <david.bosschaert@gmail.com>
2013-04-04 12:20:15 +0100

    Add test

--- hawtio-web/src/test/specs/spec/OSGiSpec.js
+++ hawtio-web/src/test/specs/spec/OSGiSpec.js
@@ -12,7 +12,7 @@ describe("OSGi", function() {
         value["Value"] = "org.foo.bar";
         var headers = {};
         headers["Export-Package"] = value;
-        var result = Osgi.parseExportPackageHeaders(headers);
+        var result = Osgi.parseManifestHeader(headers, "Export-Package");
 
         expect(Object.keys(result).length).toEqual(1);
         expect(result["org.foo.bar"]).toEqual({});
@@ -24,7 +24,7 @@ describe("OSGi", function() {
         value["Value"] = "org.foo.bar;version=1.2.3";
         var headers = {};
         headers["Export-Package"] = value;
-        var result = Osgi.parseExportPackageHeaders(headers);
+        var result = Osgi.parseManifestHeader(headers, "Export-Package");
 
         expect(Object.keys(result).length).toEqual(1);
         var expected = {Aversion: "1.2.3"};
@@ -36,7 +36,7 @@ describe("OSGi", function() {
         value["Value"] = "org.boo.far,org.foo.bar;version=1.2.3";
         var headers = {};
         headers["Export-Package"] = value;
-        var result = Osgi.parseExportPackageHeaders(headers);
+        var result = Osgi.parseManifestHeader(headers, "Export-Package");
 
         expect(Object.keys(result).length).toEqual(2);
         var expected = {Aversion: "1.2.3"};
@@ -49,15 +49,28 @@ describe("OSGi", function() {
         value["Value"] = "org.boo.far;attr=a;dir:=d";
         var headers = {};
         headers["Export-Package"] = value;
-        var result = Osgi.parseExportPackageHeaders(headers);
+        var result = Osgi.parseManifestHeader(headers, "Export-Package");
 
         expect(Object.keys(result).length).toEqual(1);
         var expected = {Aattr: "a", Ddir: "d"};
         expect(result["org.boo.far"]).toEqual(expected);
     });
 
-    it("helpers.handleExportedPackages1", function() {
-        var result = Osgi.handleExportedPackages(["org.foo.bar;1.0.0"]);
+    it("helpers.parseImportPackageHeaders1", function() {
+        var value = {};
+        value["Value"] = 'org.boo.far,org.foo.bar;version="[1.2.3,2)";resolution:=optional';
+        var headers = {};
+        headers["Import-Package"] = value;
+        var result = Osgi.parseManifestHeader(headers, "Import-Package");
+
+        expect(Object.keys(result).length).toEqual(2);
+        var expected = {Aversion: '[1.2.3,2)', Dresolution: 'optional'};
+        expect(result["org.foo.bar"]).toEqual(expected);
+        expect(result["org.boo.far"]).toEqual({});
+    });
+
+    it("helpers.handleActualPackages1", function() {
+        var result = Osgi.parseActualPackages(["org.foo.bar;1.0.0"]);
 
         expect(Object.keys(result).length).toEqual(1);
         var expected = {ReportedVersion: "1.0.0"};
