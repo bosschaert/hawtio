1eaac779b9f464f14042153149c0815841472b90
David Bosschaert <david.bosschaert@gmail.com>
2013-06-18 10:45:16 +0100

    WIP

--- hawtio-web/src/main/webapp/app/osgi/js/pid.ts
+++ hawtio-web/src/main/webapp/app/osgi/js/pid.ts
@@ -6,28 +6,6 @@ module Osgi {
         saveBtn.disabled = false;
     };
 
-    export function PidSave() {
-        var table = document.getElementById("configValues");
-
-        var els : any = table.getElementsByClassName("pid-value");
-        var result = "";
-        for (var i = 0; i < els.length; i++) {
-            result += "\n " + els[i].previousElementSibling.textContent + " " + els[i].textContent;
-        }
-
-/*
-            var mbean = getSelectionConfigAdminMBean($scope.workspace);
-            if (mbean) {
-                var jolokia = $scope.workspace.jolokia;
-                jolokia.request(
-                    {type: 'exec', mbean: mbean, operation: 'update', arguments: [$scope.pid, $scope.row]},
-                    onSuccess(populateTable));
-            }
-*/
-
-        notification("success", result);
-    };
-
     export function PidController($scope, $filter:ng.IFilterService, workspace:Workspace, $routeParams) {
         $scope.pid = $routeParams.pid;
 
@@ -51,26 +29,45 @@ module Osgi {
                 result += "\n " + els[i].previousElementSibling.textContent + " " + els[i].textContent;
             }
 
-                var props = "todo fixme";
-                    //{
-                    //foox : "bar"
-                        // {"Key": "foo", "Type": "String", "Value": "fooval"}
-                    //};
+            var props = 
+                {
+                   "a":  { "Key": "a", "Value": "abc", "Type": "String" } // ,
+                   // "felix.fileinstall.filename":  { "Key": "felix.fileinstall.filename", "Value": "file:/Users/david/clones/bosschaert_fuse_270213/fabric/fuse-fabric/target/fuse-fabric-99-master-SNAPSHOT/etc/foo.bar.cfg", "Type": "String" }
+                };
+            var args : any[] = ["foo.bar", props]; //JSON.stringify(props)];
 
 
-                var mbean = getSelectionConfigAdminMBean(workspace);
-                if (mbean) {
-                    var jolokia = workspace.jolokia;
+            var mbean = getSelectionConfigAdminMBean(workspace);
+            if (mbean) {
+                var jolokia = workspace.jolokia;
+            jolokia.request({
+                "type": "exec",
+                "mbean": mbean,
+                //"operation": "update(java.lang.String,javax.management.openmbean.TabularData)",
+                "operation": "update",
+                "arguments": args
+
+            }, {
+                method: "post", 
+                error: function(response) {
+                    notification("error", response.error);
+                },
+                success: function(response) {
+                    notification("success", response);
+                }
+            });
+/*
                     jolokia.request(
                         {type: 'exec', mbean: mbean, operation: 'update', 
                             arguments: [
-                            /* $scope.pid */ "org.ops4j.pax.url.mvn", props]},
+                            "org.ops4j.pax.url.mvn", props]},
                         {success: populateTable,
                         error: jmxError});
+*/
                         // onSuccess(populateTable), onError(jmxError));
-                }
+            }
 
-            notification("success", result);
+            // notification("success", result);
         }
 
         $scope.updateEntity = function(column, row) {
